4a23b54eb32e86768b6663bd284e1d69f5c06667
Samuel Kubai Kamau <samuel.kubai@andela.com>
2019-01-07 18:55:19 +0300

    feat(deployment): add the scripts to deploy the ACAD app
    - add the make command to start the application

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/ash
 
 printf "\n\n======================================\n"
+printf "Run the migration"
+printf "\n======================================\n\n"
+export NODE_ENV=development
+yarn run db:rollmigrate
+
+printf "\n\n======================================\n"
 printf "Start the application"
 printf "\n======================================\n\n"
 export NODE_ENV=development
